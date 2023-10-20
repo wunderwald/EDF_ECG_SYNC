55146ac5d1053406ad4b87b421989f9f1086b6ab
Moritz Wunderwald <moritzwunderwald@yahoo.de>
2023-10-20 14:08:20 +0200

    added text export

--- SyncEdfEcg/log.js
+++ SyncEdfEcg/log.js
@@ -1,10 +1,31 @@
 let LOG = true;
+let logText = '';
 
 export const SET_LOG = doLog => LOG = doLog;
 
-export const log = msg => LOG && console.log(`👀 ${msg}`);
-export const success = msg => LOG && console.log(`🥰 ${msg}`);
-export const warn = msg => LOG && console.log(`😳 ${msg}`);
-export const error = msg => LOG && console.log(`🤬 ${msg}`);
-export const info = msg => LOG && console.log(`🤓 ${msg}`);
-export const logNewline = () => LOG && console.log();
+export const getLogTxt = () => logText;
+
+export const log = msg => {
+    LOG && console.log(`👀 ${msg}`);
+    logText = `${logText}👀 ${msg}\n`;
+};
+export const success = msg => {
+    LOG && console.log(`🥰 ${msg}`);
+    logText = `${logText}🥰 ${msg}\n`;
+};
+export const warn = msg => {
+    LOG && console.log(`😳 ${msg}`);
+    logText = `${logText}😳 ${msg}\n`;
+};
+export const error = msg => {
+    LOG && console.log(`🤬 ${msg}`);
+    logText = `${logText}🤬 ${msg}\n`;
+};
+export const info = msg => {
+    LOG && console.log(`🤓 ${msg}`);
+    logText = `${logText}🤓 ${msg}\n`;
+};
+export const logNewline = () => {
+    LOG && console.log();
+    logText = `${logText}\n`;
+};
