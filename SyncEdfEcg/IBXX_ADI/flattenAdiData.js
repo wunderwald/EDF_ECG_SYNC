98eca03227bfcd849d86c36884ed675458e1e628
Moritz Wunderwald <moritzwunderwald@yahoo.de>
2023-10-20 16:24:27 +0200

    removed unused code

--- SyncEdfEcg/IBXX_ADI/flattenAdiData.js
+++
@@ -1,9 +0,0 @@
-module.exports = samples => samples.reduce((out, sample) => {
-    Object.keys(sample).forEach(key => {
-        if(!out[key]){
-            out[key] = [];
-        }
-        out[key].push(sample[key]);
-    })
-    return out;
-}, ({}));
